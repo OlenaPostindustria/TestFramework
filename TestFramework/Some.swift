52eb19d79805427c68c066ecb2285f55d10388c1
Olena Stepaniuk <olena.stepaniuk@postindustria.com>
2022-06-02 16:16:41 +0300

    add functionality

---
+++ TestFramework/Some.swift
@@ -0,0 +1,14 @@
+//
+//  Some.swift
+//  TestFramework
+//
+//  Created by Olena Stepaniuk on 02.06.2022.
+//
+
+import Foundation
+
+public class Some {
+    public func printLala() {
+        print("LALALALA")
+    }
+}
